7e9b92f4b18b1bb34e18fcdba6310eb65169bf78
Eirini <52968270+irenebritzolaki@users.noreply.github.com>
2025-10-30 12:11:28 +0200

    Add basic login component with form fields

---
+++ src/components/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {};
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleLogin}>
+          <div>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="username"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="password"
+              required
+            />
+          </div>
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Login;
